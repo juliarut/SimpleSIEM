e635213e1524119558dff055eae7973987e34896
juliarut <julia.rutfjall@gmail.com>
2025-03-12 10:32:17 +0100

    moved Logentry to new models folder

--- Data/SiemDbContext.cs
+++ Data/SiemDbContext.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using System;
 
 public class SiemDbContext : DbContext
 {
@@ -7,14 +6,4 @@ public class SiemDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlite("Data Source=siem.db");
-}
-
-public class LogEntry
-{
-    public int Id { get; set; }
-    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
-    public string Source { get; set; } 
-    public string Message { get; set; } 
-    public string Severity { get; set; }
-    public string IPAddress { get; set; }
 }
---
+++ Models/LogEntry.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class LogEntry
+{
+    public int Id { get; set; }
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    public string Source { get; set; } 
+    public string Message { get; set; }
+    public string Severity { get; set; } 
+    public string IPAddress { get; set; }
+}
